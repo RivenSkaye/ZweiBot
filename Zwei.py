cfc805c1c91b4a1fe89e8636d0c857ce0c9d64e2
RivenSkaye <pretender0906@gmail.com>
2021-04-27 23:22:13 +0200

    Cleanup and moving stuff to main.py

--- Zwei.py
+++ Zwei.py
@@ -10,6 +10,7 @@ Author: Riven Skaye
 # Type hints and function signatures
 from typing import Union, Optional
 from collections.abc import Callable
+from utils._datastores import DataStore
 # Uptime
 from datetime import datetime
 import json ### Yeet this ###
@@ -19,33 +20,18 @@ from discord.ext import commands
 # The cogs for this bot
 import cogs
 
-def _prefix(bot, msg):
-    return commands.when_mentioned_or(bot.prefixes[str(guild)].append(";"))(bot, msg) if msg.guild else commands.when_mentioned_or(";")(bot, msg)
-
 class ZweiBot(commands.Bot, case_insensitive=True): # No need to have it handle shards since we're small
-    import utils._datastores as _ds
-    def __init__(self):
+    def __init__(self, cmd_prefix: Callable, config: DataStore, database: DataStore):
+        self._config = config
         try:
-            self._config = _ds.JSONStore("data/config.json")
             assert len(self._config.get(table="config", key="token")) > 0, "No token was provided, please add it to the config."
         except AssertionError as ae:
             print(ae)
-            print("For your convenience, an empty `default_config.json` and `default_zweiDB.sdb` have been provided.")
-            print("It is recommended to copy these to the same names without the `default_` prefix for a clean start.")
             exit(1)
-        except Exception as ex:
-            print(ex)
-            print("Something went wrong trying to load data/config.json. Was it created properly?")
-            exit(1)
-        try:
-            self._db = _ds.SQLiteStore(self._config.get("db"))
-        except Exception as ex:
-            print(ex)
-            print(f"{self._config.get('db')} could not be opened. Please confirm the file exists.")
-            exit(1)
+        self._db = database
         intents = discord.Intents.default()
         intents.presences = True
-        super().__init__(command_prefix=_prefix,
+        super().__init__(command_prefix=cmd_prefix,
                        description="Hey, I'm Zwei! So I hear you need someone to do a job for you.",
                        case_insensitive=True, strip_after_prefix=True, heartbeat_timeout=180.0,
                        intents=intents)
