c32757b3b2530d116acea47f91ee0e27fea9062a
Riven Skaye <pretender0906@gmail.com>
2021-11-14 13:34:46 +0100

    First working code!

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 use std::io;
 use std::fs::File;
-use serde_json;
+use serde_json as sj;
+use serde::{Serialize, Deserialize};
 
-#[macro_use]
-extern crate lazy_static;
+//#[macro_use]
+//extern crate lazy_static;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Conf {
+    token: String,
+    owners: Vec<i64>,
+    database: String
+}
+
+fn read_conf() -> Result<Conf, io::Error> {
+    let conf_file = File::open("./data/config.json")?;
+    let reader = io::BufReader::new(conf_file);
+    let conf = sj::from_reader(reader)?;
+    Ok(conf)
+}
 
 fn main() {
+    let conf = read_conf();
+    print!("{:#?}", conf);
     // do code and get rekt
 }
