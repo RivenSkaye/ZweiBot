36aef60549c1290ef636ef157555af70cb60e6be
RivenSkaye <pretender0906@gmail.com>
2021-11-17 10:31:31 +0100

    async main, clean up code. TOML won't let you run

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use serde::{Deserialize, Serialize};
+use serde_json as sj;
+use serenity::Client;
+use std::fs::{self, File};
 use std::io::{self, Write};
 use std::path::PathBuf;
-use std::fs::File;
-use serde_json as sj;
-use serde::{Serialize, Deserialize};
 
 #[macro_use]
 extern crate lazy_static;
@@ -11,14 +12,36 @@ extern crate lazy_static;
 struct Conf {
     token: String,
     owners: Vec<i64>,
-    database: String
+    database: String,
 }
 
-fn get_data_dir() -> PathBuf{
-    std::env::current_exe().unwrap()
-    .parent().unwrap().join("data").canonicalize()
-    .or_else(|_| PathBuf::from("./data").canonicalize())
-    .unwrap()
+fn get_data_dir() -> PathBuf {
+    let pth: PathBuf = std::env::current_exe()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .to_path_buf()
+        .join("data");
+
+    let fallback = PathBuf::from("./data");
+
+    match (pth.exists(), fallback.exists()) {
+        (true, _) => pth.canonicalize().unwrap(),
+        (false, true) => fallback.canonicalize().unwrap(),
+        (false, false) => {
+            if let Ok(_) = fs::create_dir_all(&pth) {
+                pth.canonicalize().unwrap()
+            } else if let Ok(_) = fs::create_dir_all(&fallback) {
+                fallback.canonicalize().unwrap()
+            } else {
+                panic!(
+                    "Can't create {:} or {:}, please create a data folder yourself!",
+                    pth.display(),
+                    fallback.display()
+                )
+            }
+        }
+    }
 }
 
 fn create_default_conf(pth: PathBuf) -> Result<File, io::Error> {
@@ -34,7 +57,10 @@ fn read_conf() -> Result<Conf, io::Error> {
     let conf_file = File::open(pth.join("config.json"))
         .or_else(|_| File::open("./data/config.json"))
         .or_else(|_| create_default_conf(DATADIR.join("config.json")))
-        .expect(&format!("Couldn't open or create either {:}/config.json or ./data/config.json. PANIC!", pth.display()));
+        .expect(&format!(
+            "Couldn't open or create either {:}/config.json or ./data/config.json. PANIC!",
+            pth.display()
+        ));
     let reader = io::BufReader::new(conf_file);
     let conf = sj::from_reader(reader)?;
     Ok(conf)
@@ -45,6 +71,9 @@ lazy_static! {
     static ref CONF: Conf = read_conf().unwrap();
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let mut bot = Client::builder(&CONF.token).await.expect("REEEEE");
+    bot.start().await.expect("Bot no start");
     // do code and get rekt
 }
