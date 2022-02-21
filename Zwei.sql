4bf858d8b80719bb5abba67f29623f68466a5c01
Riven Skaye <pretender0906@gmail.com>
2022-02-21 17:20:15 +0100

    Cleaning stuff up

--- Zwei.sql
+++ Zwei.sql
@@ -2,28 +2,21 @@
 -- Author: Riven Skaye
 -- Project: Zwei
 
-ATTACH "Zwei.sdb" as "ZweiDB";
-BEGIN;
-CREATE TABLE IF NOT EXISTS "ZweiDB"."prefixes"(
-    "server" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    "prefix" VARCHAR(5) NOT NULL DEFAULT ";"
+CREATE TABLE IF NOT EXISTS 'prefixes'(
+    'server' INTEGER PRIMARY KEY NOT NULL,
+    'prefix' VARCHAR(5) NOT NULL DEFAULT ';'
 );
 
-CREATE TABLE IF NOT EXISTS "ZweiDB"."servertags"(
-    "tagid" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    "serverid" INTEGER NOT NULL,
-    "tagname" LONGTEXT NOT NULL,
-    CONSTRAINT "tagunique"
-        UNIQUE("serverid", "tagname")
+CREATE TABLE IF NOT EXISTS 'servertags'(
+    'tagid' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    'serverid' INTEGER NOT NULL,
+    'tagname' LONGTEXT NOT NULL,
+    UNIQUE('serverid', 'tagname') ON CONFLICT FAIL
 );
 
-CREATE TABLE IF NOT EXISTS "ZweiDB"."tagsubs"(
-    "subid" INTEGER NOT NULL,
-    "userid" INTEGER NOT NULL,
-    CONSTRAINT "subunique"
-        UNIQUE("tagid", "userid"),
-    FOREIGN KEY ("guildid") REFERENCES "servertags" ("tagid")
+CREATE TABLE IF NOT EXISTS 'tagsubs'(
+    'tagid' INTEGER NOT NULL,
+    'userid' INTEGER NOT NULL,
+    UNIQUE('tagid', 'userid') ON CONFLICT FAIL,
+    FOREIGN KEY ('tagid') REFERENCES 'servertags'('tagid')
 );
--- Oh and I figured a better way to handle the sub/tag thing!
--- A table for tags per server, with an ID and a table for users subbed
--- to each tag id. I'll handle caching it later
