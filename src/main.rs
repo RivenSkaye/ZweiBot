241b73531bf6c0863efcf420536c0e87603cd5eb
Riven Skaye <pretender0906@gmail.com>
2022-02-21 17:17:59 +0100

    Cleanup, add docs, prepare for dynamic prefixes

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use serenity::{
     framework::standard::{macros::help, Args, CommandGroup, CommandResult, HelpOptions},
     http::Http,
     model::{channel::Message, event::ResumedEvent, gateway::Ready, id::UserId},
-    prelude::*,
+    prelude::*, // also implies tokio Mutex
     utils::Colour,
     Result as SerenityResult,
 };
@@ -19,6 +19,7 @@ use std::{
 };
 
 mod commands;
+mod db;
 mod zwei_conf;
 
 #[macro_use]
@@ -34,13 +35,19 @@ impl TypeMapKey for ZweiData {
     type Value = HashMap<String, i64>;
 }
 
+pub struct ZweiPrefixes;
+impl TypeMapKey for ZweiPrefixes {
+    type Value = HashMap<u64, String>;
+}
+
 pub struct ZweiOwners;
 impl TypeMapKey for ZweiOwners {
     type Value = HashSet<UserId>;
 }
 
-struct Handler;
+use db::ZweiDBConn;
 
+struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn ready(&self, ctx: Context, ready: Ready) {
@@ -63,13 +70,69 @@ impl EventHandler for Handler {
     }
 }
 
+/// Function for dynamic prefixing. Matches the cache for custom prefixes,
+/// if none is found for this server, returns the default instead.
 #[framework::standard::macros::hook]
 async fn prefix(_ctx: &Context, _msg: &Message) -> Option<String> {
-    // Function for dynamic prefixing, currently unused.
-    // rip out its guts and Frankenstein it when the time comes!
+    // Well, polling the cache is a planned feature, actually...
     Some(String::from(";"))
 }
 
+/// Function to resolve an ID into a username. Attempts to get the proper
+/// _display name_ for the context first, but falls back to the normal
+/// username if a nickname isn't set or the context is not a guild.
+pub async fn get_name(msg: &Message, ctx: &Context, mem: UserId) -> SerenityResult<String> {
+    let guild = msg.guild(ctx).await;
+    if let Some(g) = guild {
+        let gmem = g.member(ctx, mem).await?;
+        return Ok(gmem.display_name().into_owned());
+    } else {
+        let user = mem.to_user(ctx).await?;
+        return Ok(format!("{:}#{:}", user.name, user.discriminator));
+    }
+}
+
+/// Function to resolve the textual name for the guild from the message.
+pub async fn get_guildname(msg: &Message, ctx: &Context) -> String {
+    msg.guild_id
+        .unwrap()
+        .name(ctx)
+        .await
+        .unwrap_or(String::from(""))
+}
+
+/// Function to try sending a direct message to a user. This is used to try
+/// and notify a user that they've been kicked or banned, but can also be
+/// used for otherwise contacting a user directly.
+///
+/// # Arguments
+///
+/// * `ctx` - The command context for the direct message.
+/// * `user` - The `UserId` for the user to DM.
+/// * `title` - Anything that implements [`std::fmt::Display`].
+/// * `msg` - The message body to send, must implement [`std::fmt::Display`].
+///
+/// Examples can be found in [`commands::modtools`] in `kick` and `ban`.
+pub async fn try_dm(
+    ctx: &Context,
+    user: UserId,
+    title: impl std::fmt::Display,
+    msg: impl std::fmt::Display,
+) -> SerenityResult<()> {
+    let color =
+        Colour::from(u32::from_str_radix(&zwei_conf::CONF.ok_color.replace("#", ""), 16).unwrap());
+    let chan = user.create_dm_channel(ctx).await?;
+    chan.send_message(ctx, |mes| {
+        mes.embed(|e| e.color(color).title(title).description(msg))
+    })
+    .await?;
+    Ok(())
+}
+
+/// Function to sanitize text and escape markdown. Especially useful when
+/// dealing with clowns that think it's funny to make a bot post broken
+/// messages rather than correctly displaying their name.
+/// Can be used to sanitize any arbitrary string slice.
 pub fn sanitize_txt(txt: &str) -> String {
     // Thanks a lot to Acrimon for making this not shit
     let mut sanitized = String::with_capacity(txt.len() * 4 / 3);
@@ -89,41 +152,15 @@ pub fn sanitize_txt(txt: &str) -> String {
     sanitized
 }
 
-pub async fn get_name(msg: &Message, ctx: &Context, mem: UserId) -> SerenityResult<String> {
-    let guild = msg.guild(ctx).await;
-    if let Some(g) = guild {
-        let gmem = g.member(ctx, mem).await?;
-        return Ok(gmem.display_name().into_owned());
-    } else {
-        let user = mem.to_user(ctx).await?;
-        return Ok(format!("{:}#{:}", user.name, user.discriminator));
-    }
-}
-
-pub async fn try_dm(
-    ctx: &Context,
-    user: UserId,
-    title: impl std::fmt::Display,
-    msg: impl std::fmt::Display,
-) -> SerenityResult<()> {
-    let color =
-        Colour::from(u32::from_str_radix(&zwei_conf::CONF.ok_color.replace("#", ""), 16).unwrap());
-    let chan = user.create_dm_channel(ctx).await?;
-    chan.send_message(ctx, |mes| {
-        mes.embed(|e| e.color(color).title(title).description(msg))
-    })
-    .await?;
-    Ok(())
-}
-
-pub async fn get_guildname(msg: &Message, ctx: &Context) -> String {
-    msg.guild_id
-        .unwrap()
-        .name(ctx)
-        .await
-        .unwrap_or(String::from(""))
-}
-
+/// Central function to send an embed indicating something went wrong.
+/// This is the version that's not too descriptive.
+///
+/// # Arguments
+///
+/// * `ctx` - Command context
+/// * `msg` - The message that invoked the command leading to the error.
+/// * `errtxt` - Anything implementing `std::fmt::Display` as textual
+///              indication of what went wrong.
 pub async fn send_err(
     ctx: &Context,
     msg: &Message,
@@ -132,6 +169,18 @@ pub async fn send_err(
     Ok(send_err_titled(ctx, msg, "Something went wrong!", errtxt).await?)
 }
 
+/// Central function to send an embed indicating something went wrong.
+/// Allows for setting a title as well, which could be used to provide more
+/// information about what went wrong or how a command was used incorrectly.
+///
+/// # Arguments
+///
+/// * `ctx` - Command context
+/// * `msg` - The message that invoked the command leading to the error.
+/// * `title` - Anything that implements [`std::fmt::Display`], like the
+///             command name or some other descriptive heading.
+/// * `errtxt` - Anything implementing [`std::fmt::Display`] as textual
+///              indication of what went wrong.
 pub async fn send_err_titled(
     ctx: &Context,
     msg: &Message,
@@ -148,6 +197,17 @@ pub async fn send_err_titled(
     Ok(())
 }
 
+/// Function to indicate successful processing of a command. Uses Zwei's
+/// pretty colors to indicate everything is good.
+///
+/// # Arguments
+///
+/// * `ctx` - Command context
+/// * `msg` - The message that invoked the command that was completed.
+/// * `title` - Anything that implements [`std::fmt::Display`], like the
+///             command name or some other descriptive heading.
+/// * `msgtxt` - Anything implementing [`std::fmt::Display`] as textual message
+///              returning information about command execution to the user.
 pub async fn send_ok(
     ctx: &Context,
     msg: &Message,
@@ -222,6 +282,10 @@ async fn main() {
         .intents(GatewayIntents::all())
         .await
         .expect("Zwei is feeling special today");
+    let arcsqlite = Arc::new(Mutex::new(
+        rusqlite::Connection::open(zwei_conf::DATADIR.join(&conf.database))
+            .expect("Can't open the database!"),
+    ));
     {
         let mut data = bot.data.write().await;
         data.insert::<ShardManagerContainer>(bot.shard_manager.clone());
@@ -230,6 +294,7 @@ async fn main() {
         zd.insert("id".to_string(), i64::from(self_id));
         data.insert::<ZweiData>(zd);
         data.insert::<ZweiOwners>(owners.clone());
+        data.insert::<ZweiDBConn>(arcsqlite);
     }
     let shard_manager = bot.shard_manager.clone();
     tokio::spawn(async move {
