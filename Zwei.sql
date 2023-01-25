1c0ffeeb08cb551b27be76c41a0253c4d276f85e
Riven Skaye <riven@tae.moe>
2023-01-25 15:10:05 +0100

    SQL comments while I'm at it

--- Zwei.sql
+++ Zwei.sql
@@ -2,14 +2,20 @@
 -- Author: Riven Skaye
 -- Project: Zwei
 
+-- PRAGMAs for sqlite3
+-- use FKs
 PRAGMA foreign_keys = ON;
+-- Write Ahead Logging, for increased throughput from async code
 PRAGMA journal_mode = WAL;
 
+
+-- Prefixes the bot will respond to
 CREATE TABLE IF NOT EXISTS 'prefixes'(
     'server' INTEGER PRIMARY KEY NOT NULL,
     'prefix' VARCHAR(5) NOT NULL DEFAULT ';'
 );
 
+-- Tags registered for a guild
 CREATE TABLE IF NOT EXISTS 'servertags'(
     'tagid' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     'serverid' INTEGER NOT NULL,
@@ -17,6 +23,7 @@ CREATE TABLE IF NOT EXISTS 'servertags'(
     UNIQUE('serverid', 'tagname') ON CONFLICT FAIL
 );
 
+-- Users subscribed to certain tags
 CREATE TABLE IF NOT EXISTS 'tagsubs'(
     'tagid' INTEGER NOT NULL,
     'userid' INTEGER NOT NULL,
@@ -24,6 +31,7 @@ CREATE TABLE IF NOT EXISTS 'tagsubs'(
     FOREIGN KEY ('tagid') REFERENCES 'servertags'('tagid') ON DELETE CASCADE
 );
 
+-- Warnings emitted per user per guild
 CREATE TABLE IF NOT EXISTS 'warnings'(
     'warnid' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     'serverid' INTEGER NOT NULL,
